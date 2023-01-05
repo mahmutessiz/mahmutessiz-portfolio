92984b4aa4c987405965ead091df715c7d2868f6
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-01-05 14:29:06 +0300

    e-mail icon added

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,7 +3,9 @@ export default {
   data() {
     return {
       sideBarItems: {
-        listStyles: "my-6 transition-transform hover:rotate-12",
+        listStyles: "my-4 transition-transform hover:rotate-12",
+        asideStyles:
+          "fixed mt-40 ml-4 flex w-14 items-center justify-center rounded-2xl bg-primary-bg-black py-10 px-2 shadow-lg shadow-black",
       },
     };
   },
@@ -11,15 +13,16 @@ export default {
 </script>
 
 <template>
-  <aside
-    class="fixed mt-40 ml-4 flex h-1/2 w-14 items-center justify-center rounded-2xl bg-primary-bg-black px-2 shadow-lg shadow-black"
-  >
+  <aside :class="sideBarItems.asideStyles">
     <ul class="flex list-none flex-col">
       <li :class="sideBarItems.listStyles">
         <img src="../public/github(1).png" alt="Github" />
       </li>
       <li :class="sideBarItems.listStyles">
         <img src="../public/linkedin(2).png" alt="linkedin" />
+      </li>
+      <li :class="sideBarItems.listStyles">
+        <img src="../public/email.png" alt="e-mail" />
       </li>
       <li :class="sideBarItems.listStyles">
         <img src="../public/twitter(1).png" alt="twitter" />
