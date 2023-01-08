876546f63f5161956d7764f5ecd79a9b906807eb
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-01-08 18:48:28 +0300

    *

--- src/components/side-components/ChangeStyleButtons.vue
+++ src/components/side-components/ChangeStyleButtons.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="mt-4 flex items-end justify-end gap-4 p-6">
     <li class="cursor-pointer">
-      <img class="h-12 hover:scale-110" src="../public/extended.png" alt="" />
+      <img class="h-12 hover:scale-110" src="../public/Extended.png" alt="" />
     </li>
     <li class="cursor-pointer">
       <img class="m-1 h-12 hover:scale-110" src="../public/rect.png" alt="" />
