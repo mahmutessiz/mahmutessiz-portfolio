9ba163abe769ce78b892f87e1eb3330c0b99c3db
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-01-09 15:20:09 +0300

    add some functionality to project items

--- src/components/side-components/ProjectSectionItems.vue
+++ src/components/side-components/ProjectSectionItems.vue
@@ -2,29 +2,53 @@
 export default {
   data() {
     return {
+      deneme: "text-white",
+      listIn: ["1", "2", "3", "4", "5"],
       itemStyle: "h-80 w-80 border-2 border-solid border-white",
+      itemStyle2: "w-full h-60 border",
+      ulItemOrderStyle2:
+        "flex flex-wrap flex-col items-center justify-center gap-4 py-9",
+      ulItemOrderStyle: "flex flex-wrap items-center justify-center gap-4 py-9",
     };
+  },
+  methods: {
+    change1() {
+      return (this.deneme = this.itemStyle);
+    },
+    change2() {
+      return (this.deneme = this.itemStyle2);
+    },
   },
 };
 </script>
 
 <template>
+  <!-- xxxxxxxxxxx  BUTTONS xxxxxxxxxxxxx -->
+
   <ul class="mt-4 flex items-end justify-end gap-4 p-6">
-    <li class="cursor-pointer">
-      <img class="h-12 hover:scale-110" src="../public/Extended.png" alt="" />
+    <li class="cursor-pointer" @click="change2">
+      <img
+        class="h-12 hover:scale-110 active:scale-110"
+        src="../public/Extended.png"
+        alt=""
+      />
     </li>
-    <li class="cursor-pointer">
-      <img class="m-1 h-12 hover:scale-110" src="../public/rect.png" alt="" />
+    <li class="cursor-pointer" @click="change1">
+      <img
+        class="m-1 h-12 hover:scale-110 active:scale-110"
+        src="../public/rect.png"
+        alt=""
+      />
     </li>
   </ul>
 
+  <!-- xxxxxxxxxxxx  ITEMS xxxxxxxxxxxx -->
+
   <div class="w-2/3 border-white">
-    <ul class="flex flex-wrap items-center justify-center gap-4 py-9">
-      <li :class="itemStyle"></li>
-      <li :class="itemStyle"></li>
-      <li :class="itemStyle"></li>
-      <li :class="itemStyle"></li>
-      <li :class="itemStyle"></li>
+    <ul :class="ulItemOrderStyle">
+      <li :class="deneme" v-for="item in listIn" :key="item">
+        {{ item }}
+      </li>
     </ul>
   </div>
 </template>
