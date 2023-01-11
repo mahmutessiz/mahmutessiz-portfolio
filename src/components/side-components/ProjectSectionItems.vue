e7b18ccd0a1dad05681567eb5c55cacea8bc9427
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-01-11 13:07:36 +0300

    project items structure

--- src/components/side-components/ProjectSectionItems.vue
+++ src/components/side-components/ProjectSectionItems.vue
@@ -1,22 +1,60 @@
 <script>
-import thimage from "../images/email.png";
+import pomodoro from "../images/pomodoro.png";
 export default {
   data() {
     return {
-      lastStyle: "w-full h-60 border rounded",
+      lastStyle: "w-full flex gap-4 text-white border rounded",
+      itemStyle: "w-80 border-2 border-solid border-white",
+      itemStyle2: "w-full flex gap-4 text-white border rounded",
+      ulItemOrderStyle: "flex flex-wrap items-center justify-center gap-4 py-9",
+
       listIn: [
         {
           key: "1",
-          imageSrc: thimage,
+          imageSrc: pomodoro,
+          descriptionContainerStyle:
+            "w-full border bg-primary-bg-darkslategray py-4 text-primary-text-thistle",
+          projectImgStyle:
+            "w-full cursor-pointer transition-all duration-500 hover:opacity-80",
+          projectName: "Deneme",
         },
-        { key: "2", imageSrc: thimage },
-        { key: "3", imageSrc: thimage },
-        { key: "4", imageSrc: thimage },
-        { key: "5", imageSrc: thimage },
+        {
+          key: "2",
+          imageSrc: pomodoro,
+          descriptionContainerStyle:
+            "w-full border bg-primary-bg-darkslategray py-4 text-primary-text-thistle",
+          projectImgStyle:
+            "w-full cursor-pointer transition-all duration-500 hover:opacity-80",
+          projectName: "Deneme",
+        },
+        {
+          key: "3",
+          imageSrc: pomodoro,
+          descriptionContainerStyle:
+            "w-full border bg-primary-bg-darkslategray py-4 text-primary-text-thistle",
+          projectImgStyle:
+            "w-full cursor-pointer transition-all duration-500 hover:opacity-80",
+          projectName: "Deneme",
+        },
+        {
+          key: "4",
+          imageSrc: pomodoro,
+          descriptionContainerStyle:
+            "w-full border bg-primary-bg-darkslategray py-4 text-primary-text-thistle",
+          projectImgStyle:
+            "w-full cursor-pointer transition-all duration-500 hover:opacity-80",
+          projectName: "Deneme",
+        },
+        {
+          key: "5",
+          imageSrc: pomodoro,
+          descriptionContainerStyle:
+            "w-full border bg-primary-bg-darkslategray py-4 text-primary-text-thistle",
+          projectImgStyle:
+            "w-full cursor-pointer transition-all duration-500 hover:opacity-80",
+          projectName: "Deneme",
+        },
       ],
-      itemStyle: "h-80 w-80 border-2 border-solid border-white",
-      itemStyle2: "w-full h-60 border",
-      ulItemOrderStyle: "flex flex-wrap items-center justify-center gap-4 py-9",
     };
   },
   methods: {
@@ -55,12 +93,16 @@ export default {
   <div class="w-3/4 border-white">
     <ul :class="ulItemOrderStyle">
       <li :class="lastStyle" v-for="item in listIn" :key="item.key">
-        <img :src="item.imageSrc" class="w-1/2" alt="" />
-        <div class="mt-8 h-32 w-full border bg-slate-400">
-          <h3 class="text-center">Deneme</h3>
+        <img
+          :src="item.imageSrc"
+          class="w-96 cursor-pointer transition-all duration-500 hover:opacity-80"
+          alt=""
+        />
+        <div class="">
+          <h3 class="text-center text-lg font-bold">{{ item.projectName }}</h3>
           <hr />
           <br />
-          <p class="overflow-hidden">
+          <p class="px-2">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
             incidunt officia unde, eos corporis tenetur totam
           </p>
