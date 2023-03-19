73524ed2880c7a96d4574d4c2c8d44c8a45ec614
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-03-19 14:29:04 +0300

    main section column added

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,15 +1,12 @@
 <template>
   <section
-    class="flex h-screen w-full items-center justify-center bg-primary-bg-darkslategray py-40 text-primary-text-thistle"
+    class="flex h-screen w-full flex-wrap items-center justify-center gap-4 bg-primary-bg-darkslategray py-40 px-12 text-primary-text-thistle"
   >
-    <div class="-mt-32 w-3/4 max-w-screen-lg md:w-3/4">
+    <div class="w-3/4 max-w-screen-lg px-16 md:w-1/2">
       <br />
-      <h1 class="text-center text-3xl font-bold md:text-5xl">
-        I'm Mahmut Eşsiz
-      </h1>
-      <hr />
+      <h1 class="text-left text-3xl font-bold md:text-5xl">I'm Mahmut Eşsiz</h1>
       <br />
-      <p class="text-center text-sm md:text-lg">
+      <p class="text-left text-sm md:text-lg">
         I am a web developer with a strong foundation in technologies such as
         HTML, CSS and JavaScript. I am highly motivated and always seeking to
         improve my skills and stay up-to-date with the latest industry trends. I
@@ -17,6 +14,9 @@
         web applications, and I am excited to continue building my career in
         this field.
       </p>
+      <br />
+      <span class="font-mono">Stack: vue, js, tailwind, nuxt</span>
     </div>
+    <div class="-mt-24 h-96 w-1/3 bg-primary-bg-black"></div>
   </section>
 </template>
