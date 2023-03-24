08909dbbc6da8825ce9ee94fdec6d6aaf92fb374
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-03-24 15:21:48 +0300

    add purple shadow

--- src/views/ImageGalery.vue
+++ src/views/ImageGalery.vue
@@ -11,17 +11,18 @@ export default {
   },
   beforeMount() {
     this.routeName = this.route.params.id;
-    console.log(this.routeName);
   },
 };
 </script>
 
 <template>
-  <ul class="flex w-full flex-wrap items-center justify-center gap-4 px-4 py-8">
+  <ul
+    class="flex w-full flex-wrap items-center justify-center gap-8 px-4 pt-8 pb-24"
+  >
     <li
       v-for="data in ImagesData[routeName]"
       :key="data"
-      class="w-3/4 transition-all duration-200 hover:scale-110 md:w-1/2"
+      class="w-3/4 shadow-lg shadow-purple-500 transition-all duration-200 hover:scale-110 md:w-1/2"
     >
       <img :src="data" alt="project image" />
     </li>
