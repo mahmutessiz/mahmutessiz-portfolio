b7e70dd19a9957d23bf851fa98b93f9c7d51fce3
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-05-18 21:35:26 +0300

    add new project

--- src/views/ImageGalery.vue
+++ src/views/ImageGalery.vue
@@ -17,22 +17,24 @@ export default {
 </script>
 
 <template>
-  <nav
-    class="left-8 top-20 flex justify-center py-9 font-bold text-white md:fixed"
-  >
-    <RouterLink to="/">
-      <button class="rounded-lg border p-4 hover:scale-95">Go Back</button>
-    </RouterLink>
-  </nav>
-  <ul
-    class="flex w-full flex-wrap items-center justify-center gap-8 px-4 pt-8 pb-24"
-  >
-    <li
-      v-for="data in ImagesData[routeName]"
-      :key="data"
-      class="w-3/4 shadow-lg shadow-purple-500 transition-all duration-200 hover:scale-110 md:w-1/2"
+  <section class="min-h-screen w-full">
+    <nav
+      class="left-8 top-20 flex justify-center py-9 font-bold text-white md:fixed"
     >
-      <img :src="data" alt="project image" />
-    </li>
-  </ul>
+      <RouterLink to="/">
+        <button class="rounded-lg border p-4 hover:scale-95">Go Back</button>
+      </RouterLink>
+    </nav>
+    <ul
+      class="flex w-full flex-wrap items-center justify-center gap-8 px-4 pt-8 pb-24"
+    >
+      <li
+        v-for="data in ImagesData[routeName]"
+        :key="data"
+        class="w-3/4 shadow-lg shadow-purple-500 transition-all duration-200 hover:scale-110 md:w-1/2"
+      >
+        <img :src="data" alt="project image" />
+      </li>
+    </ul>
+  </section>
 </template>
