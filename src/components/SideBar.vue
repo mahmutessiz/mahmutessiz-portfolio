3af04ce587d774ae94a5339c6f5c89763cd507ed
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-07-04 12:51:08 +0300

    .

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -47,6 +47,7 @@ export default {
           href="../assets/Resume_MahmutEssiz.pdf"
           class="text-primary-text-thistle"
           title="CV"
+          download
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
