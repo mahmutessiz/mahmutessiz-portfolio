f0d808791f77b950c005db195db9022121a485ec
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-08-18 17:25:02 +0300

    Change titles based on the page

--- src/router/index.js
+++ src/router/index.js
@@ -7,25 +7,40 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
+      meta: {
+        title: "Mahmut Eşsiz",
+      },
       component: HomeView,
     },
     {
       path: "/galery/:id",
       name: "imageGalery",
+      meta: {
+        title: "Mahmut Eşsiz | Project images",
+      },
       component: () => import("../views/ImageGalery.vue"),
     },
     {
       path: "/blog",
       name: "blog",
+      meta: {
+        title: "Mahmut Eşsiz | Blog",
+      },
       component: () => import("../views/BlogView.vue"),
     },
     {
       path: "/blog-post/:name",
       name: "blog-post",
+      meta: {
+        title: "Mahmut Eşsiz | Blog Post",
+      },
       component: () => import("../views/BlogPostsView.vue"),
     },
     {
       path: "/:pathMatch(.*)*",
+      meta: {
+        title: "Mahmut Eşsiz | 404 Error",
+      },
       name: "notFound",
       component: () => import("@/views/ErrorView.vue"),
     },
@@ -34,6 +49,11 @@ const router = createRouter({
     // always scroll to top
     return { top: 0, behavior: "smooth" };
   },
+});
+
+router.beforeEach((to,from,next)=>{
+  document.title = `${to.meta.title}`;
+  next();
 });
 
 export default router;
